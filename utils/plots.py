eb3cccb2cdab21226994fd3613f02350ac96104d
Murat Ali A <77502485+muratali016@users.noreply.github.com>
2022-09-03 14:05:07 +0300

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -59,13 +59,20 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    #cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    m1,m2,m3,m4=c1[0],c1[1],c2[0],c2[1]
+    t1=(m1+m3)/2
+    t2=(m2+m4)/2
+
+    middle = (int(t1),int(t2)-50)
+    
     if label:
-        tf = max(tl - 1, 1)  # font thickness
+        tf = 1
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        #cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)
+        #cv2.putText(img, label, (c1[0],c1[1]), 0, tl, [225, 255, 255], thickness=5, lineType=cv2.LINE_AA)
+    return middle
 
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):
